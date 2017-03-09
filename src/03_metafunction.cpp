510a47aeb131bbcbceaeca2916ab513084f35729
Gil Ho, Jang <jmxexpert@hotmail.com>
2017-03-10 01:02:20 +0900

    added some test cases.

--- src/03_metafunction.cpp
+++ src/03_metafunction.cpp
@@ -1,9 +1,87 @@
 #include "catch.hpp"
 
-#include "type.h"
+#include <cstddef>
+
+#include "02_type_is.h"
 
 
 //==============================================================================
+// numeric calculation
+//==============================================================================
+
+// constexpr recursive_factorial function for C++11
+constexpr std::size_t recursive_factorial(std::size_t i)
+{
+    return (0 == i || 1 == i) ? 1
+                              : (i * recursive_factorial(i - 1));
+}
+
+// constexpr iterative_factorial function for C++14
+constexpr std::size_t iterative_factorial(std::size_t i)
+{
+    std::size_t val = 1;
+    for ( ; i; --i) {
+        val *= i;
+    }
+    return val;
+}
+
+// factorial_t metafunction
+template <std::size_t i>
+struct factorial_t
+{
+    constexpr static auto value = factorial_t<i - 1>::value * i;
+};
+
+template <>
+struct factorial_t<0>
+{
+    constexpr static auto value = 1;
+};
+
+template <>
+struct factorial_t<1>
+{
+    constexpr static auto value = 1;
+};
+
+
+TEST_CASE("factorial metafunction", "[tmp]")
+{
+    //==== recursive factorial
+    auto rf = recursive_factorial(5);
+    //static_assert(rf == 1 * 2 * 3 * 4 * 5);   // expected compiler error
+    REQUIRE(rf == 1 * 2 * 3 * 4 * 5);           // need to check at runtime
+
+    constexpr auto crf = recursive_factorial(5);
+    static_assert(crf == 1 * 2 * 3 * 4 * 5);
+    static_assert(recursive_factorial(5) == 1 * 2 * 3 * 4 * 5);
+
+    //==== iterative factorial
+    auto iterf = iterative_factorial(5);
+    //static_assert(iterf == 1 * 2 * 3 * 4 * 5);    // expected compiler error
+    REQUIRE(iterf == 1 * 2 * 3 * 4 * 5);            // need to check at runtime
+
+    constexpr auto citerf = iterative_factorial(5);
+    static_assert(citerf == 1 * 2 * 3 * 4 * 5);
+    static_assert(iterative_factorial(5) == 1 * 2 * 3 * 4 * 5);
+
+    //==== template metafunction factorial
+    static_assert(factorial_t<0>::value == 1);
+    static_assert(factorial_t<5>::value == 1 * 2 * 3 * 4 * 5);
+
+    // You can call constexpr function inside the template argument list.
+    static_assert(
+        factorial_t<
+            iterative_factorial(3)  // <== 6
+        >::value == 1 * 2 * 3 * 4 * 5 * 6
+    );
+}
+
+//==============================================================================
+// type calculation
+//==============================================================================
+
 // runtime function f
 template <typename Arg>
 Arg f(Arg a)
@@ -22,7 +100,7 @@ struct F
 template <typename Arg>
 using f_t = typename F<Arg>::type;
 
-TEST_CASE("metafunction basic concept", "[tmp]")
+TEST_CASE("type calculation metafunction", "[tmp]")
 {
     auto a = f(100);
 
