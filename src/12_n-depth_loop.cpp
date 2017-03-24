4a8909b73ea0e78eda3fcc836a4d2273dd1c8091
Gil Ho, Jang <jmxexpert@hotmail.com>
2017-03-24 18:15:13 +0900

    updated test cases.

--- src/12_n-depth_loop.cpp
+++ src/12_n-depth_loop.cpp
@@ -7,6 +7,9 @@
 #include <iostream>
 
 
+#if __clang_major__ >= 4    // due to C++17 std::apply
+
+
 template <typename T, T LoopDepth>
 struct loop_index_generator
 {
@@ -55,9 +58,55 @@ auto loop(T... i)
 
 TEST_CASE("n-depth loop", "[tmp]")
 {
+    /*
     loop(2, 2)(
         [](auto i, auto j) {
             std::cout << '(' << i << ", " << j << ")\n";
         }
     );
+    */
+
+    // This is somewhat like the following:
+    //      for (int i = 0; i < 3; ++i)
+    //          for (int j = 0; j < 3; ++j)
+    //              for (int k = 0; k < 3; ++k)
+    //                  f(i, j, k);
+    loop(3, 3, 3)(
+        [](auto i, auto j, auto k) {
+            std::cout << '(' << i << ", " << j << ", " << k << ")\n";
+        }
+    );
+
+    /*
+    loop(2, 3, 4, 5)(
+        [](auto i, auto j, auto k, auto l) {
+            std::cout << '(' << i << ", " << j << ", " << k << ", " << l << ")\n";
+        }
+    );
+    */
+
+    // TODO: add following function call support.
+
+    // The function has smaller number of parameters than the loop depth.
+    /*
+    loop(3, 3, 3)(
+        [](auto i, auto j) {
+            std::cout << '(' << i << ", " << j << ")\n";
+        }
+    );
+    */
+
+    // The function don't use the loop index at all.
+    /*
+    loop(3, 3, 3)(
+        []{
+            std::cout << "just do something.\n";
+        }
+    );
+    */
+
+    // ...
 }
+
+
+#endif  // __clang_major__ >= 4
