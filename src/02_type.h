40c3ff2a0e33f248510d695c66c8b0de2177cb3a
Gil Ho, Jang <jmxexpert@hotmail.com>
2017-03-24 15:47:27 +0900

    added an empty struct for general type list container.

--- src/02_type.h
+++ src/02_type.h
@@ -15,4 +15,15 @@ template <typename T>
 using type_is = identity<T>;
 
 
+// type list
+template <typename... T>
+struct type_list
+{ };
+
+
+// type list alias for convenience
+template <typename... T>
+using TL = type_list<T...>;
+
+
 #endif // CPP_TMP_STUDY_TYPE_H
